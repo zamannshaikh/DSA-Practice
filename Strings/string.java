a5f06de3c5cec380d85284ae72eca1a54286b251
Zaman Shaikh <zamanshaikh2048@gmail.com>
2026-06-24 15:00:26 +0530

    case toggle

--- Strings/string.java
+++ Strings/string.java
@@ -19,29 +19,59 @@
 //2] Pallindrome
 
 
-class Main {
-    public static void main(String[] args) {
-        String str = "madam";
-        int s = 0;
-        int e = str.length() - 1;
-        boolean isPallindrome = true;
+// class Main {
+//     public static void main(String[] args) {
+//         String str = "madam";
+//         int s = 0;
+//         int e = str.length() - 1;
+//         boolean isPallindrome = true;
         
-        while (s < e) {
+//         while (s < e) {
           
-            if (str.charAt(s) != str.charAt(e)) {
-                isPallindrome = false;
-                break;
-            }
+//             if (str.charAt(s) != str.charAt(e)) {
+//                 isPallindrome = false;
+//                 break;
+//             }
             
-            s++;
-            e--;
-        }
+//             s++;
+//             e--;
+//         }
         
         
-        if (isPallindrome) {
-            System.out.println("Pallindrome");
-        } else {
-            System.out.println("Not Pallindrome");
-        }
-    }
-}
+//         if (isPallindrome) {
+//             System.out.println("Pallindrome");
+//         } else {
+//             System.out.println("Not Pallindrome");
+//         }
+//     }
+// }
+
+
+
+//3] Toggle character of strings : abBDcS => ABbdCs
+
+
+
+// class Main{
+//     public static void main(String[] args){
+//         String str="abBDcS";
+//         char[] ans=new char[str.length()];
+//         for(int i=0;i<str.length();i++){
+//             int ascii=(int) str.charAt(i);
+//             if(ascii>=65 && ascii<=90){
+//                 int newVal =ascii+32;
+//                 char chars=(char) newVal;
+//                 ans[i]=chars;
+//             }
+//             else {
+//                 int newVal=ascii-32;
+//                 char chars=(char) newVal ;
+//                 ans[i]=chars;
+//             }
+            
+            
+//         }
+//         String str2=new String(ans);
+//         System.out.print(str2);
+//     }
+// }
