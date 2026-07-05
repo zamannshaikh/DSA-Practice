a221437f4e4a2ae04e62a0a93736c3b55b4a8d68
Zaman Shaikh <zamanshaikh2048@gmail.com>
2026-07-05 15:40:20 +0530

    Implement frequency counting using HashMap in Java
    Add a Java program to count frequency of elements using HashMap.

---
+++ HashMap.java
@@ -0,0 +1,23 @@
+//1] count frequency using HashMap
+
+
+// import java.util.HashMap;
+// import java.util.Map;
+// class Main{
+//     public static void main(String[] args){
+//         int arr[]={1,2,3,2,1,23,4,5,4,4,5,2};
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         for(int i:arr){
+//             if(map.containsKey(i)){
+//                 map.put(i,map.get(i)+1);
+//             }
+//             else map.put(i,1);
+//         }
+        
+//         for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+//             int key=entry.getKey();
+//             int val=entry.getValue();
+//             System.out.println(key+"=>"+" "+val);
+//         }
+//     }
+// }
