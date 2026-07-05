1f9a3c0e680726965e06ca5d9dcb3373bcb73a4b
Zaman Shaikh <zamanshaikh2048@gmail.com>
2026-07-05 16:49:58 +0530

    first letter to appear twice

--- HashMap.java
+++ HashMap.java
@@ -91,3 +91,39 @@
         
 //     }
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+//]4 first letter to appear twice (leet code)
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public char repeatedCharacter(String s) {
+//         HashMap<Character,Integer> map=new HashMap<>();
+//         for(int i=0;i<s.length();i++){
+//             char ch=s.charAt(i);
+//             map.put(ch,map.getOrDefault(ch,0)+1);
+//             if(map.get(ch)==2) return ch;
+//         }
+//         return s.charAt(0); 
+        
+//     }
+// }
