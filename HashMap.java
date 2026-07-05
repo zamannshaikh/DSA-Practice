db2c43d4a9213386fbea6519fdb856a4c618e4fc
Zaman Shaikh <zamanshaikh2048@gmail.com>
2026-07-05 16:32:34 +0530

    two sum

--- HashMap.java
+++ HashMap.java
@@ -60,3 +60,34 @@
 //         }
 //     }
 // }
+
+
+
+
+
+
+
+//3] two sum(leetcode)
+
+
+
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         HashMap<Integer,Integer> map=new HashMap<>();
+//         int ans[]={-1,-1};
+//         for(int i=0;i<nums.length;i++){
+//             if(map.containsKey(target-nums[i])){
+//                 ans[0]=map.get(target-nums[i]);
+//                 ans[1]=i;
+//                 return ans;
+
+        
+//             } else{
+//                 map.put(nums[i],i);
+//             }
+//         }
+//         return ans;
+        
+//     }
+// }
