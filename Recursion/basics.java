02745d72babf48b36af291c7e297eb3bc28e961e
Zaman Shaikh <zamanshaikh2048@gmail.com>
2026-07-07 07:16:09 +0530

    recursive function to print numbers from n to 1

---
+++ Recursion/basics.java
@@ -0,0 +1,21 @@
+//1] print number from n to 1 using recursion
+
+
+
+
+
+// class Main {
+//     public static void main(String[] args) {
+//         int n=10;
+//         count(n);
+//     }
+    
+    
+    
+//         public static void count(int n){
+//             if(n==0) return;
+//             System.out.println(n);
+//             count(n-1);
+            
+//         }
+// }
