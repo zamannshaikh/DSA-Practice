d3bdace3b29b4edf60d3104a8d4181dc041939ba
Zaman Shaikh <zamanshaikh2048@gmail.com>
2026-07-08 06:29:37 +0530

    sum of n numbers using recursion

--- Recursion/basics.java
+++ Recursion/basics.java
@@ -19,3 +19,43 @@
             
 //         }
 // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//2] sum of n numbers using recursion 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Main {
+//     public static void main(String[] args) {
+//         System.out.print(sum(5));
+//     }
+//     public static int sum(int n){
+//         if(n==1) return 1;
+//         return n+sum(n-1);
+//     }
+// }
