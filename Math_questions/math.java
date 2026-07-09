986bf4345ae9fe9aa0c59fd837dfbf95f79ea0f2
zamannshaikh <zamanshaikh2048@gmail.com>
2026-07-09 07:19:05 +0530

    gcd Method 1

---
+++ Math_questions/math.java
@@ -0,0 +1,12 @@
+// //1] gcb (Method 1)
+// class Main {
+//     public static void main(String[] args) {
+//         int a=16,b=24;
+//         for(int i=Math.min(a,b);i>=1;i--){
+//             if(a%i==0 && b%i==0){
+//                 System.out.print(i);
+//                 break;
+//             }
+//         }
+//     }
+// }
