c040253ca7d152b6570aeade49bed14ca2e1b6d9
zamannshaikh <zamanshaikh2048@gmail.com>
2026-07-09 07:29:04 +0530

    method 2 of gcd

--- Math_questions/math.java
+++ Math_questions/math.java
@@ -10,3 +10,25 @@
 //         }
 //     }
 // }
+
+
+
+
+
+
+
+
+
+
+// 2] Method 2
+// class Main {
+//     public static void main(String[] args) {
+  
+//         int a= 16,b=24;
+//         while(a !=b){
+//             if(a>b) a=a-b;
+//             else b=b-a;
+//         }
+//         System.out.print(a);
+//     }
+// }
