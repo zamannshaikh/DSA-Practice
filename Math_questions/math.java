20f81017ad218c0992569506bc21ad2465f985ec
zamannshaikh <zamanshaikh2048@gmail.com>
2026-07-11 07:09:51 +0530

    Sieve of Eratosthenes

--- Math_questions/math.java
+++ Math_questions/math.java
@@ -84,3 +84,45 @@
 //     }
     
 // }
+
+
+
+
+
+
+
+
+//5] count of prime numbers in n(Sieve of Eratosthenes)
+
+
+
+
+
+
+
+// import java.util.Scanner;
+// import java.util.Arrays;
+
+
+// public class math{
+//     public static void main(String[] args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+//         boolean arr[]=new boolean[n+1];
+//         Arrays.fill(arr,true);
+//         for(int i=2;i<=Math.sqrt(n);i++){
+//             if(arr[i]){
+//                 for(int j=i*i;j<=n;j+=i){
+//                     arr[j]=false;
+//                 }
+
+//             }
+//         }
+//         for(int i=2;i<arr.length;i++){
+//             if(arr[i]){
+//                 System.out.print(i+" ");
+//             }
+            
+//         }
+//     }
+// }
